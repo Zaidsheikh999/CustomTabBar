91bd20952cb43457fe44367b383ebe081db42e4c
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-17 13:36:49 +0500

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Zaid Sheikh on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
